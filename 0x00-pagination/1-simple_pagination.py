339e012c51b320a98d896f309005ab9b59b1b9f5
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-02 12:02:55 +0300

    add documentation.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -18,7 +18,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Create the Cached dataset.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
